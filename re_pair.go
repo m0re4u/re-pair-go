6af89a4e708a41fdb1d9a495747b52ff14916804
Michiel van der Meer <michiel@vandermeer.ai>
2020-02-28 17:45:23 +0100

    Add unit hover detection

--- re_pair.go
+++ re_pair.go
@@ -37,13 +37,29 @@ func (*DefaultScene) Setup(u engo.Updater) {
 	world.AddSystem(&common.RenderSystem{})
 	world.AddSystem(&common.MouseSystem{})
 
-	us := &systems.UnitSpawner{}
-	world.AddSystem(us)
+	// Custom cursor
 	world.AddSystem(&systems.MouseFollower{})
 
-	us.SpawnUnitAtLocation(200, 200)
-	us.SpawnUnitAtLocation(300, 200)
-	us.SpawnUnitAtLocation(400, 200)
+	// Units
+	us := &systems.UnitSpawner{}
+	world.AddSystem(us)
+	unit := us.SpawnUnitAtLocation(200, 200)
+	unit2 := us.SpawnUnitAtLocation(300, 300)
+
+	for _, system := range world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&unit.BasicEntity, &unit.RenderComponent, &unit.SpaceComponent)
+			sys.Add(&unit2.BasicEntity, &unit2.RenderComponent, &unit2.SpaceComponent)
+		case *common.MouseSystem:
+			sys.Add(&unit.BasicEntity, &unit.MouseComponent, &unit.SpaceComponent, &unit.RenderComponent)
+			sys.Add(&unit2.BasicEntity, &unit2.MouseComponent, &unit2.SpaceComponent, &unit2.RenderComponent)
+		case *systems.UnitSpawner:
+			sys.Add(&unit)
+			sys.Add(&unit2)
+		}
+	}
+
 }
 
 func main() {
--- systems/unit_spawner.go
+++ systems/unit_spawner.go
@@ -9,104 +9,68 @@ import (
 	"github.com/EngoEngine/engo/common"
 )
 
-const unitScaling = 5
-
-// MouseTracker info about mouse
-type MouseTracker struct {
-	ecs.BasicEntity
-	common.MouseComponent
-}
-
 // Unit unit defition
 type Unit struct {
 	ecs.BasicEntity
 	common.RenderComponent
 	common.SpaceComponent
+	common.MouseComponent
 }
 
 // UnitSpawner takes care of unit spawning
 type UnitSpawner struct {
-	world *ecs.World
-
-	mouseTracker MouseTracker
+	world      *ecs.World
+	AliveUnits []*Unit // slice of pointers to all units
 }
 
 // Remove is called whenever an Entity is removed from the scene, and thus from this system
 func (*UnitSpawner) Remove(ecs.BasicEntity) {}
 
-// New is the initialisation of the System
+// Add a unit to the system
+func (us *UnitSpawner) Add(u *Unit) {
+	us.AliveUnits = append(us.AliveUnits, u)
+}
+
+// New is the initialisation of the UnitSpawner System
 func (us *UnitSpawner) New(w *ecs.World) {
 	us.world = w
 	fmt.Println("UnitSpawner was added to the Scene")
-
-	us.mouseTracker.BasicEntity = ecs.NewBasic()
-	us.mouseTracker.MouseComponent = common.MouseComponent{Track: true}
-
-	for _, system := range w.Systems() {
-		switch sys := system.(type) {
-		case *common.MouseSystem:
-			sys.Add(&us.mouseTracker.BasicEntity, &us.mouseTracker.MouseComponent, nil, nil)
-		}
-	}
 }
 
-// SpawnUnitAtLocation spawn new unit at the given location
-func (us *UnitSpawner) SpawnUnitAtLocation(x float32, y float32) {
+// NewUnit create a new unit entity
+func (us *UnitSpawner) NewUnit(posx float32, posy float32) Unit {
 	texture, err := common.LoadedSprite("textures/unit.png")
 	if err != nil {
 		log.Println("Unable to load texture: " + err.Error())
 	}
-
-	unit := Unit{
-		BasicEntity: ecs.NewBasic(),
-		RenderComponent: common.RenderComponent{
-			Drawable: texture,
-			Scale:    engo.Point{X: unitScaling, Y: unitScaling},
-		},
-		SpaceComponent: common.SpaceComponent{
-			Position: engo.Point{X: x, Y: y},
-			Width:    texture.Width(),
-			Height:   texture.Height(),
-		},
+	unit := Unit{BasicEntity: ecs.NewBasic()}
+	unit.RenderComponent = common.RenderComponent{
+		Drawable: texture,
+		Scale:    engo.Point{X: 8, Y: 8},
 	}
-	for _, system := range us.world.Systems() {
-		switch sys := system.(type) {
-		case *common.RenderSystem:
-			sys.Add(&unit.BasicEntity, &unit.RenderComponent, &unit.SpaceComponent)
-		}
+	unit.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{X: posx, Y: posy},
+		Width:    texture.Width() * unit.RenderComponent.Scale.X,
+		Height:   texture.Height() * unit.RenderComponent.Scale.Y,
+		Rotation: 0,
 	}
+	return unit
 
+}
+
+// SpawnUnitAtLocation spawn new unit at the given location
+func (us *UnitSpawner) SpawnUnitAtLocation(x float32, y float32) Unit {
+	unit := us.NewUnit(x, y)
+	return unit
 }
 
 // Update is ran every frame, with `dt` being the time
 // in seconds since the last frame
 func (us *UnitSpawner) Update(dt float32) {
-	if engo.Input.Button("SpawnUnit").JustPressed() {
-		fmt.Println("Player spawned unit")
-
-		unit := Unit{BasicEntity: ecs.NewBasic()}
-
-		unit.SpaceComponent = common.SpaceComponent{
-			Position: engo.Point{X: us.mouseTracker.MouseComponent.MouseX - 20, Y: us.mouseTracker.MouseComponent.MouseY - 20},
-			Width:    8,
-			Height:   8,
-		}
-
-		texture, err := common.LoadedSprite("textures/unit.png")
-		if err != nil {
-			log.Println("Unable to load texture: " + err.Error())
-		}
-
-		unit.RenderComponent = common.RenderComponent{
-			Drawable: texture,
-			Scale:    engo.Point{X: unitScaling, Y: unitScaling},
-		}
-
-		for _, system := range us.world.Systems() {
-			switch sys := system.(type) {
-			case *common.RenderSystem:
-				sys.Add(&unit.BasicEntity, &unit.RenderComponent, &unit.SpaceComponent)
-			}
+	for _, u := range us.AliveUnits {
+		fmt.Println("")
+		if u.MouseComponent.Hovered {
+			fmt.Println("HOVER on unit:", u.BasicEntity.ID())
 		}
 	}
 }
