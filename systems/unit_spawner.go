630d574659013abb2b7271d757391ce0c4b5e305
Michiel van der Meer <michiel@vandermeer.ai>
2020-03-03 14:46:32 +0100

    Added circle under units for selection

--- systems/unit_spawner.go
+++ systems/unit_spawner.go
@@ -2,6 +2,7 @@ package systems
 
 import (
 	"fmt"
+	"image/color"
 	"log"
 
 	"github.com/EngoEngine/ecs"
@@ -15,6 +16,16 @@ type Unit struct {
 	common.RenderComponent
 	common.SpaceComponent
 	common.MouseComponent
+	position engo.Point
+	selected bool
+	shadow   Shadow
+}
+
+// Shadow render unit shadow
+type Shadow struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
 }
 
 // UnitSpawner takes care of unit spawning
@@ -44,28 +55,43 @@ func (us *UnitSpawner) newUnit(posx float32, posy float32) Unit {
 		log.Println("Unable to load texture: " + err.Error())
 	}
 	unit := Unit{BasicEntity: ecs.NewBasic()}
+	unit.position = engo.Point{X: posx, Y: posy}
 	unit.RenderComponent = common.RenderComponent{
 		Drawable: texture,
 		Scale:    engo.Point{X: 8, Y: 8},
 	}
 	unit.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{X: posx, Y: posy},
+		Position: engo.Point{X: unit.position.X, Y: unit.position.Y},
 		Width:    texture.Width() * unit.RenderComponent.Scale.X,
 		Height:   texture.Height() * unit.RenderComponent.Scale.Y,
-		Rotation: 0,
 	}
 
+	unit.shadow = Shadow{BasicEntity: ecs.NewBasic()}
+	unit.shadow.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{X: unit.position.X, Y: unit.position.Y},
+		Width:    texture.Width() * unit.RenderComponent.Scale.X,
+		Height:   texture.Height() * unit.RenderComponent.Scale.Y,
+	}
+	unit.shadow.RenderComponent = common.RenderComponent{Drawable: common.Circle{}, Color: color.RGBA{0, 0, 0, 255}}
+
 	return unit
+}
+
+func (us *UnitSpawner) moveUnit(unit *Unit, transx float32, transy float32) {
+	unit.SpaceComponent.Position.X += transx
+	unit.SpaceComponent.Position.Y += transy
+	unit.shadow.SpaceComponent.Position.X += transx
+	unit.shadow.SpaceComponent.Position.Y += transy
 }
 
 // SpawnUnitAtLocation spawn new unit at the given location
 func (us *UnitSpawner) SpawnUnitAtLocation(x float32, y float32) {
 	unit := us.newUnit(x, y)
-
 	for _, system := range us.world.Systems() {
 		switch sys := system.(type) {
 		case *common.RenderSystem:
 			sys.Add(&unit.BasicEntity, &unit.RenderComponent, &unit.SpaceComponent)
+			sys.Add(&unit.shadow.BasicEntity, &unit.shadow.RenderComponent, &unit.shadow.SpaceComponent)
 		case *common.MouseSystem:
 			sys.Add(&unit.BasicEntity, &unit.MouseComponent, &unit.SpaceComponent, &unit.RenderComponent)
 		case *UnitSpawner:
@@ -79,10 +105,8 @@ func (us *UnitSpawner) SpawnUnitAtLocation(x float32, y float32) {
 // in seconds since the last frame
 func (us *UnitSpawner) Update(dt float32) {
 	for _, u := range us.AliveUnits {
-		if u.MouseComponent.Hovered {
-			fmt.Println("HOVER on unit:", u.BasicEntity.ID())
-			u.SpaceComponent.Position.X += 10
-
+		if u.MouseComponent.Clicked {
+			us.moveUnit(u, 10, 0)
 		}
 	}
 }
